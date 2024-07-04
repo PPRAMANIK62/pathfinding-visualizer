02e89743038af0b9cf9423d7f457568ef8060eb8
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-07-04 22:43:02 +0530

    speed context done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { PathfindingProvider } from "./context/PathfindingContext";
+import { SpeedProvider } from "./context/SpeedContext";
+import { TileProvider } from "./context/TileContext";
 
 const App = () => {
   return (
     <PathfindingProvider>
-      <div className="h-screen w-screen bg-blue-500 text-3xl font-bold underline">
-        Hello world
-      </div>
+      <TileProvider>
+        <SpeedProvider>
+          <div className="h-screen w-screen bg-blue-500 text-3xl font-bold underline">
+            Hello world
+          </div>
+        </SpeedProvider>
+      </TileProvider>
     </PathfindingProvider>
   );
 };
