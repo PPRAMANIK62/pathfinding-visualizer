cf7578f97ffa5f57e603a66b84d18f5470d04180
Purbayan Pramanik <purbayanpramanik62@gmail.com>
2024-07-04 22:31:22 +0530

    pathfinding context done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { PathfindingProvider } from "./context/PathfindingContext";
+
 const App = () => {
   return (
-    <div className="h-screen w-screen bg-blue-500 text-3xl font-bold underline">
-      App
-    </div>
+    <PathfindingProvider>
+      <div className="h-screen w-screen bg-blue-500 text-3xl font-bold underline">
+        Hello world
+      </div>
+    </PathfindingProvider>
   );
 };
 
---
+++ src/context/PathfindingContext.tsx
@@ -0,0 +1,52 @@
+import React, { createContext, useState } from "react";
+import {
+  END_TILE_CONFIGURATION,
+  START_TILE_CONFIGURATION,
+} from "../utils/constants";
+import { createGrid } from "../utils/helper";
+import { AlgorithmType, GridType, MazeType } from "../utils/types";
+
+export interface PathfindingContextInterface {
+  algorithm: AlgorithmType;
+  setAlgorithm: (algorithm: AlgorithmType) => void;
+  maze: MazeType;
+  setMaze: (maze: MazeType) => void;
+  grid: GridType;
+  setGrid: (grid: GridType) => void;
+  isGraphVisualized: boolean;
+  setIsGraphVisualized: (isGraphVisualized: boolean) => void;
+}
+
+export const PathfindingContext = createContext<
+  PathfindingContextInterface | undefined
+>(undefined);
+
+export const PathfindingProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [algorithm, setAlgorithm] = useState<AlgorithmType>("BFS");
+  const [maze, setMaze] = useState<MazeType>("NONE");
+  const [grid, setGrid] = useState<GridType>(
+    createGrid(START_TILE_CONFIGURATION, END_TILE_CONFIGURATION),
+  );
+  const [isGraphVisualized, setIsGraphVisualized] = useState<boolean>(false);
+
+  return (
+    <PathfindingContext.Provider
+      value={{
+        algorithm,
+        setAlgorithm,
+        maze,
+        setMaze,
+        grid,
+        setGrid,
+        isGraphVisualized,
+        setIsGraphVisualized,
+      }}
+    >
+      {children}
+    </PathfindingContext.Provider>
+  );
+};
